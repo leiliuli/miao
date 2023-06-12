4f2c0b220b1b3be922b7a5f65dce06b7a7ba3df0
fanao <fan@qq.com>
2023-06-12 15:20:22 +0800

    2023年6月12日15:20:19

--- lodash/leiliuli-lodash.js
+++ lodash/leiliuli-lodash.js
@@ -48,13 +48,14 @@ var leiliuli = {
     }
     return ary;
   },
-     fill : function  (value, start=0, end = this.length){
-      for (let i = start; i < end;i++){
-        this[i] = value
-      }
-      return this;
-  },
 
+     fill : function  (value, start=0, end = value.length){
+      for (let i = start; i < end;i++){
+       value [i] = value
+      }
+      return value;
+  },
+  
 }
 
 
